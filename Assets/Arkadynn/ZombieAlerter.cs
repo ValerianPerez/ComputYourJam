9ae11c99cccfc69d632a94431837be50e8b75a76
Valérian Perez <valerian.perez@gmail.com>
2018-10-28 00:22:31 +0200

    Revert "Merge branch 'master' of https://github.com/ValerianPerez/ComputYourJam"
    This reverts commit 32bca46254bce852fc2ff3aebb6e6a9df8b4ba41, reversing
    changes made to 796aa44e321b901b47686c801ec48038124b5cbf.

--- Assets/Arkadynn/ZombieAlerter.cs
+++ Assets/Arkadynn/ZombieAlerter.cs
@@ -7,27 +7,10 @@ public class ZombieAlerter : MonoBehaviour {
 
     public int alertLevel = 0;
 
-    public int hitPoint = 20;
-
-    public GameObject rootObject;
-
-    void Start() {
-        if (rootObject == null)
-            rootObject = gameObject;
-    }
-
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Zombie")) {
             Debug.Log("Yummy Found");
             other.GetComponent<AgentScript>().NotifyYummy(transform, alertLevel);
-        }
-    }
-
-    public void TakeAHit(int damage) {
-        hitPoint -= damage;
-        if (hitPoint <= 0) {
-            Debug.Log("Agent " + rootObject.name + " is down !!!! : ");
-            rootObject.SetActive(false);
         }
     }
 }
