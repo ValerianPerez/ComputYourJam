a67d0f14fe02e10f38fcf80ddbf3fb780d216231
Phenix <vlt31@hotmail.fr>
2018-10-27 03:13:00 +0200

    Light system
    Added light system, lights msut be close enough to a battery to light up
    Call the public method CheckBattery on each light eah time a battery is built/destroyed.
    (Sprites are ugly, msut change it)

--- Assets/Scripts/BatteryCheck.cs
+++ Assets/Scripts/BatteryCheck.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class BatteryCheck : MonoBehaviour {
 
+    public float range = 5.0f;
+    private Light myLight;
+
 	// Use this for initialization
 	void Start () {
-		
+        myLight = GetComponent<Light>();
+        CheckBattery();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
 	}
+
+	public void CheckBattery() {
+		GameObject[] batteries = GameObject.FindGameObjectsWithTag("Battery");
+        myLight.enabled = false;
+        foreach (GameObject battery in batteries) {
+            Debug.Log(Vector2.Distance(this.transform.position, battery.transform.position));
+            if(Vector2.Distance(this.transform.position, battery.transform.position) < range)
+            {
+                myLight.enabled = true;
+                break;
+            }
+        }
+	}
+
+    void OnDrawGizmos()
+    {
+        //https://answers.unity.com/questions/842981/draw-2d-circle-with-gizmos.html
+    }
 }
