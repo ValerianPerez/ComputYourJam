eba9b9aa3672e6982857ea725ecf5050d358d370
Valérian Perez <valerian.perez@gmail.com>
2018-10-27 04:49:09 +0200

    Add PlayerController and multipad

---
+++ Assets/Scripts/PlayersManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayersManager : MonoBehaviour {
+
+    private List<PlayerControler> players;
+
+	// Use this for initialization
+	void Start () {
+        GameObject[] p = GameObject.FindGameObjectsWithTag("Player");
+        players = new List<PlayerControler>();
+
+        for (int i = 0; i < p.Length; i++)
+        {
+            players.Add(p[i].GetComponent<PlayerControler>().SetUp(i+1));
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
