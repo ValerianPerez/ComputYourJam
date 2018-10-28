47935948b113f91a30c35bd2dcb9184d3b26f446
Valérian Perez <valerian.perez@gmail.com>
2018-10-28 20:57:13 +0100

    last push mathafakka

--- Assets/Arkadynn/SpawnZombies.cs
+++ Assets/Arkadynn/SpawnZombies.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SpawnZombies : MonoBehaviour {
 
     public Transform[] spawnPoints;
     public GameObject[] zombies;
+
+    private List<GameObject> zombiesAlive;
 
     private int[] quantities;
     private float[] delays;
@@ -14,11 +17,14 @@ public class SpawnZombies : MonoBehaviour {
     private float timer = 0f;
 
     private bool spawnInProgress = false;
+    private bool spawnFinishedAndUnresolved = false;
 
     void Start() {
         int[] qties = { 10, 10, 10, 10 };
         float[] del = { 2, 2, 2, 2 };
         int[] bulk = { 3, 3, 3, 3 };
+
+        zombiesAlive = new List<GameObject>();
 
         SpawnWave(qties, del, bulk);
     }
@@ -34,8 +40,17 @@ public class SpawnZombies : MonoBehaviour {
     }
 
     void Update() {
-        if (!spawnInProgress)
-            return;
+        if (!spawnInProgress && spawnFinishedAndUnresolved)
+        {
+            bool isClear = true;
+            foreach (var item in zombiesAlive)
+            {
+                isClear &= !item.GetComponent<AgentScript>().isDead;
+            }
+
+            if (isClear)
+                SceneManager.LoadScene(2);
+        }
 
         spawnInProgress = false;
 
@@ -55,7 +70,7 @@ public class SpawnZombies : MonoBehaviour {
             quantities[i] -= qty;
 
             for(int j = 0; j < qty; ++j) {
-                Instantiate(zombies[Random.Range(0,zombies.Length)], spawnPoints[i].position, Quaternion.Euler(new Vector3(90, 0, 0)));
+                zombiesAlive.Add(Instantiate(zombies[Random.Range(0, zombies.Length)], spawnPoints[i].position, Quaternion.Euler(new Vector3(90, 0, 0))));
             }
 
             timer = Time.deltaTime;
